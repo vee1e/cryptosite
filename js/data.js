2fe9e7fa4c5f62bf12a7b2c1c6e0c0ee80db363f
vee1e <vermalucky2004@gmail.com>
2025-08-16 13:29:14 +0530

    align stuff properly

--- js/data.js
+++ js/data.js
@@ -204,7 +204,8 @@ const achievements = {
         { event: "RVCE X IIT Bombay Trust Lab CTF 2024",           result: "2nd Place",      prize: "30,000 INR",   date: "June 2024" },
         { event: "CRAW CTF at Prometeo, IIT Jodhpur",              result: "1st Place",      prize: "25,000 INR",   date: "February 2025" },
         { event: "GIT X IIT Bombay Trust Lab CTF 2025",            result: "1st Place",      prize: "25,000 INR",   date: "May 2025" },
-        { event: "BSides Bangalore B5 CTF, W2 CTF",                result: "1st, 2nd Place", prize: "22,000 INR",   date: "June 2025" },
+        { event: "BSides Bangalore B5 CTF",                        result: "1st Place",      prize: "15,000 INR", date: "June 2025" },
+        { event: "BSides Bangalore W2 CTF",                        result: "2nd Place",      prize: "7,000 INR", date: "June 2025" },
         { event: "Securicum Independence Day CTF",                 result: "1st Place",      prize: "10,000 INR",   date: "August 2025" }
     ],
     hackathons: [
@@ -212,8 +213,8 @@ const achievements = {
         { event: "Smart India Hackathon 2023 at NIET Noida", result: "1st Place", prize: "1,00,000 INR", date: "December 2023" },
         { event: "Smart India Hackathon 2024 at IIT Jammu",  result: "1st Place", prize: "1,00,000 INR", date: "December 2024" },
         { event: "Cyber Crimes Conclave, IISc Bangalore",    result: "2nd Place", prize: "1,00,000 INR", date: "February 2024" },
-        { event: "AICS ICONIP CyberAI Cup 2023",             result: "1st Place", prize: "3000 NZD",     date: "November 2023" },
-        { event: "AICS ICONIP CyberAI Cup 2024",             result: "3rd Place", prize: "1000 NZD",     date: "November 2024" }
+        { event: "AICS ICONIP CyberAI Cup 2023",             result: "1st Place", prize: "3,000 NZD",    date: "November 2023" },
+        { event: "AICS ICONIP CyberAI Cup 2024",             result: "3rd Place", prize: "1,000 NZD",    date: "November 2024" }
     ]
 };
 
